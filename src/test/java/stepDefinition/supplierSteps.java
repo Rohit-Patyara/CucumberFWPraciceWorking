267c07824780b8073e36c1841bf7e442ee2f0868
DELL <DELL@DESKTOP-7C911OD>
2024-08-23 18:03:22 +0530

    Added New file and methods

---
+++ src/test/java/stepDefinition/supplierSteps.java
@@ -0,0 +1,26 @@
+package stepDefinition;
+
+public class supplierSteps {
+
+	public void add() {
+		int a = 10;
+		int b = 5;
+		int res = a + b;
+		System.out.println(res);
+	}
+
+	public void sub() {
+		int a = 10;
+		int b = 5;
+		int res = a - b;
+		System.out.println(res);
+	}
+
+	public void mul() {
+		int a = 10;
+		int b = 5;
+		int res = a * b;
+		System.out.println(res);
+	}
+
+}
