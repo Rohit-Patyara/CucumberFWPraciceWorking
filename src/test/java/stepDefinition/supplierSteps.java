bc87108ed28b882602778a27d08dc1ff1c8d3f26
DELL <DELL@DESKTOP-7C911OD>
2024-08-23 18:10:53 +0530

    Modified Values

--- src/test/java/stepDefinition/supplierSteps.java
+++ src/test/java/stepDefinition/supplierSteps.java
@@ -3,22 +3,22 @@ package stepDefinition;
 public class supplierSteps {
 
 	public void add() {
-		int a = 10;
-		int b = 5;
+		int a = 1011;
+		int b = 524;
 		int res = a + b;
 		System.out.println(res);
 	}
 
 	public void sub() {
-		int a = 10;
-		int b = 5;
+		int a = 1420;
+		int b = 5224;
 		int res = a - b;
 		System.out.println(res);
 	}
 
 	public void mul() {
-		int a = 10;
-		int b = 5;
+		int a = 102;
+		int b = 5787;
 		int res = a * b;
 		System.out.println(res);
 	}
